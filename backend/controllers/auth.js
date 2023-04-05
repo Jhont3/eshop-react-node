75647c34aa83cffdb22ea25c167948f376fc7a2e
Jhonatan <jhont.escobar17@gmail.com>
2023-04-05 01:10:38 -0500

    change validation user responsability with req body, hash pass and default salt sync

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,17 +1,43 @@
 const { response } = require('express')     //to access to suggestions while code
+const bcrypt = require('bcryptjs');
+const User = require('../models/User')
 
-
-const signUp = (req, res = response) => {
+const signUp = async (req, res = response) => {
 
     const { name, email, password } = req.body
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
+    try {
+        let user = await User.findOne({ email });
+        console.log(user);
+
+        if ( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The user already exists'
+            })
+        }
+
+        user = new User( req.body ); 
+
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt )
+
+        await user.save()
+    
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Something went wrong, please contact the admin'
+        })
+    }
+
 }
 
 const signIn = (req, res = response ) => {
